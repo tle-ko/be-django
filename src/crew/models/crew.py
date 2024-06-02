7bfc0e6cf2afdb44f2bc2269dbe5765fb288c171
Hepheir <hepheir@gmail.com>
2024-06-03 03:13:22 +0900

    refactor(crew): CrewRecruitment 모델을 Crew에 병합시킴

--- src/crew/models/crew.py
+++ src/crew/models/crew.py
@@ -44,6 +44,42 @@ class Crew(models.Model):
             '유저가 사용 가능한 언어를 입력해주세요.'
         ),
     )
+    max_member = models.IntegerField(
+        help_text=(
+            '크루 최대 인원을 입력해주세요.'
+        ),
+        validators=[
+            MinValueValidator(1),
+            # TODO: 최대 인원 제한
+        ],
+    )
+    is_boj_user_only = models.BooleanField(
+        help_text=(
+            '백준 아이디 필요 여부를 입력해주세요.'
+        ),
+        default=False,
+    )
+    min_boj_tier = models.IntegerField(
+        help_text=(
+            '최소 백준 레벨을 입력해주세요. ',
+            '0: Unranked, 1: Bronze V, 2: Bronze IV, ..., 6: Silver V, ..., 30: Ruby I'
+        ),
+        choices=BOJLevel.choices,
+        null=True,
+        default=None,
+    )
+    max_boj_tier = models.IntegerField(
+        help_text=(
+            '최대 백준 레벨을 입력해주세요. ',
+            '0: Unranked, 1: Bronze V, 2: Bronze IV, ..., 6: Silver V, ..., 30: Ruby I'
+        ),
+        validators=[
+            # TODO: 최대 레벨이 최소 레벨보다 높은지 검사
+        ],
+        choices=BOJLevel.choices,
+        null=True,
+        default=None,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -92,28 +128,6 @@ class CrewMemberRequest(models.Model):
         null=True,
         blank=True,
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-class CrewRecruitment(models.Model):
-    crew = models.OneToOneField(
-        Crew,
-        on_delete=models.CASCADE,
-        related_name='opening',
-        help_text=(
-            '크루를 입력해주세요.'
-        ),
-        unique=True,
-    )
-    max_member = models.IntegerField(
-        help_text=(
-            '크루 최대 인원을 입력해주세요.'
-        ),
-        validators=[
-            MinValueValidator(1),
-            # TODO: 최대 인원 제한
-        ],
-    )
     tags = models.JSONField(
         help_text=(
             '태그를 입력해주세요.'
@@ -122,28 +136,4 @@ class CrewRecruitment(models.Model):
             # TODO: 태그 형식 검사
         ],
     )
-    is_boj_user_only = models.BooleanField(
-        help_text=(
-            '백준 아이디 필요 여부를 입력해주세요.'
-        ),
-        default=False,
-    )
-    min_boj_tier = models.IntegerField(
-        help_text=(
-            '최소 백준 레벨을 입력해주세요. ',
-            '0: Unranked, 1: Bronze V, 2: Bronze IV, ..., 6: Silver V, ..., 30: Ruby I'
-        ),
-        choices=BOJLevel.choices,
-        default=BOJLevel.U,
-    )
-    max_boj_tier = models.IntegerField(
-        help_text=(
-            '최대 백준 레벨을 입력해주세요. ',
-            '0: Unranked, 1: Bronze V, 2: Bronze IV, ..., 6: Silver V, ..., 30: Ruby I'
-        ),
-        validators=[
-            # TODO: 최대 레벨이 최소 레벨보다 높은지 검사
-        ],
-        choices=BOJLevel.choices,
-        default=BOJLevel.R1,
-    )
+    created_at = models.DateTimeField(auto_now_add=True)
