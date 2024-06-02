1525e208902a0cb85520775ea78f147cd8ecd9f8
Hepheir <hepheir@gmail.com>
2024-06-02 23:46:26 +0900

    feat(crew): Add CrewOpening model and related fields

--- src/crew/models.py
+++ src/crew/models.py
@@ -1,3 +1,5 @@
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from user.models import User
@@ -43,3 +45,69 @@ class Crew(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class CrewOpening(models.Model):
+    crew = models.OneToOneField(
+        Crew,
+        on_delete=models.CASCADE,
+        related_name='opening',
+        help_text=(
+            '크루를 입력해주세요.'
+        ),
+        unique=True,
+    )
+    max_member = models.IntegerField(
+        help_text=(
+            '크루 최대 인원을 입력해주세요.'
+        ),
+        validators=[
+            MinValueValidator(1),
+            # TODO: 최대 인원 제한
+        ],
+    )
+    tags = models.JSONField(
+        help_text=(
+            '태그를 입력해주세요.'
+        ),
+        validators=[
+            # TODO: 태그 형식 검사
+        ],
+    )
+    allowed_languages = models.JSONField(
+        help_text=(
+            '허용 언어를 입력해주세요. ',
+            '언어의 아이디를 입력해주세요.'
+        ),
+        validators=[
+            # TODO: 언어 아이디 검사
+        ],
+    )
+    is_boj_user_only = models.BooleanField(
+        help_text=(
+            '백준 아이디 필요 여부를 입력해주세요.'
+        ),
+        default=False,
+    )
+    min_boj_tier = models.IntegerField(
+        help_text=(
+            '최소 백준 레벨을 입력해주세요. ',
+            '0: Unranked, 1: Bronze V, 2: Bronze IV, ..., 6: Silver V, ..., 30: Ruby I'
+        ),
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ],
+        default=0,
+    )
+    max_boj_tier = models.IntegerField(
+        help_text=(
+            '최대 백준 레벨을 입력해주세요. ',
+            '0: Unranked, 1: Bronze V, 2: Bronze IV, ..., 6: Silver V, ..., 30: Ruby I'
+        ),
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(30),
+        ],
+        default=30,
+    )
