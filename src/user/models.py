70012f9bc2ab775e3daf614f627ca4a9edf77f76
Hepheir <hepheir@gmail.com>
2024-06-02 18:27:30 +0900

    feat(user): create "User" model

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,28 @@
+from django.contrib import auth
 from django.db import models
 
-# Create your models here.
+
+class User(auth.models.User):
+    REQUIRED_FIELDS = [
+        'email',
+        'username',
+        'password',
+    ]
+
+    email = models.EmailField(
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    image = models.ImageField(
+        upload_to='user_images/',
+        null=True
+    )
+    boj_id = models.CharField(
+        max_length=100, # TODO: 추후 조사 필요
+        unique=True,
+        help_text=(
+            '백준 아이디를 입력해주세요.'
+        ),
+        null=True,
+    )
