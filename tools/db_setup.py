3f5d7ea3adba8c5ab65ea7751a1837534bffb357
Hepheir <hepheir@gmail.com>
2024-06-03 15:57:57 +0900

    chore: Add script to load tags from JSON file and update DSA model

---
+++ tools/db_setup.py
@@ -0,0 +1,57 @@
+"""
+Use this like:
+
+$ python manage.py shell < tools/db_setup.py
+"""
+
+import dataclasses
+import json
+from typing import List
+
+
+@dataclasses.dataclass
+class DisplayName:
+    language: str
+    name: str
+    short: str
+
+
+@dataclasses.dataclass
+class Alias:
+    alias: str
+
+
+@dataclasses.dataclass
+class Tag:
+    key: str
+    isMeta: bool
+    bojTagId: int
+    problemCount: int
+    displayNames: List[DisplayName]
+    aliases: List[Alias]
+
+
+
+def load_tags(file='tools/tags.json') -> List[Tag]:
+    with open(file) as f:
+        raw_tags = json.load(f)
+    tags = []
+    for item in raw_tags['items']:
+        tag = Tag(**item)
+        tag.displayNames = [DisplayName(**display_name) for display_name in item["displayNames"]]
+        tag.aliases = [Alias(**alias) for alias in item["aliases"]]
+        tags.append(tag)
+    return tags
+
+
+from django.db.transaction import atomic
+from core.models import DSA
+
+
+with atomic():
+    for tag in load_tags():
+        dsa = DSA.objects.get_or_create(key=tag.key)[0]
+        dsa.boj_tag_id = tag.bojTagId
+        dsa.name_ko = next(filter(lambda x: x.language == 'ko', tag.displayNames)).name
+        dsa.name_en = next(filter(lambda x: x.language == 'en', tag.displayNames)).name
+        dsa.save()
