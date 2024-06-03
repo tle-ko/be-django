9057214dbf61757518cef096fcf9738f2f12ce33
Hepheir <hepheir@gmail.com>
2024-06-03 19:50:15 +0900

    refactor(core): Increase max length for name_ko and name_en fields in Tag model

--- src/core/models.py
+++ src/core/models.py
@@ -17,26 +17,27 @@ class Tag(models.Model):
             '부모 알고리즘 태그를 입력해주세요.'
         ),
         null=True,
+        blank=True,
     )
     key = models.CharField(
-        max_length=20,
+        max_length=50,
         unique=True,
         help_text=(
             '알고리즘 태그 키를 입력해주세요. (최대 20자)'
         ),
     )
     name_ko = models.CharField(
-        max_length=20,
+        max_length=50,
         unique=True,
         help_text=(
-            '알고리즘 태그 이름(국문)을 입력해주세요. (최대 20자)'
+            '알고리즘 태그 이름(국문)을 입력해주세요. (최대 50자)'
         ),
     )
     name_en = models.CharField(
-        max_length=20,
+        max_length=50,
         unique=True,
         help_text=(
-            '알고리즘 태그 이름(영문)을 입력해주세요. (최대 20자)'
+            '알고리즘 태그 이름(영문)을 입력해주세요. (최대 50자)'
         ),
     )
 
