999ef931e852c034f795ebf6a2b3a855b2d6ba68
Hepheir <hepheir@gmail.com>
2024-06-19 16:21:01 +0900

    feat(problem.serializers): create `ProblemAnalysisSerializer`

--- src/problem/serializers.py
+++ src/problem/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework.serializers import *
 
+from core.serializers import TagSerializer
 from user.serializers import UserSerializer
 
 from .models import *
 
 
+class ProblemAnalysisSerializer(ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ProblemAnalysis
+        fields = '__all__'
+        extra_kwargs = {
+            'created_at': {'read_only': True},
+            'problem': {'write_only': True},
+        }
+
+
 class ProblemSerializer(ModelSerializer):
     user = UserSerializer(read_only=True)
+    analysis = ProblemAnalysisSerializer(many=True, read_only=True)
 
     class Meta:
         model = Problem
