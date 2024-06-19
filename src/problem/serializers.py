63d14f0936b84dc82f704ba839ec8f6491611c38
Hepheir <hepheir@gmail.com>
2024-06-19 16:20:52 +0900

    feat(problem.serializers): create `ProblemSerializer`

--- src/problem/serializers.py
+++ src/problem/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework.serializers import *
 
+from user.serializers import UserSerializer
+
 from .models import *
+
+
+class ProblemSerializer(ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Problem
+        fields = '__all__'
+        extra_kwargs = {
+            'created_at': {'read_only': True},
+            'updated_at': {'read_only': True},
+        }
