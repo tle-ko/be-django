968d54ae031ed797832d30fc6a4747ab69557d2f
Hepheir <hepheir@gmail.com>
2024-06-20 18:48:32 +0900

    feat(config.views): create `NACLExceptionReporter`
    ... which reports debugging details only to ALLOWED_HOSTS

---
+++ src/config/views.py
@@ -0,0 +1,20 @@
+
+from django.conf import settings
+from django.views import debug
+
+
+class NACLExceptionReporter(debug.ExceptionReporter):
+    def __init__(self, request, exc_type, exc_value, tb, is_email=False):
+        super().__init__(request, exc_type, exc_value, tb, is_email)
+
+
+    def get_traceback_data(self) -> dict:
+        """Return a dictionary containing traceback information."""
+        if self._get_domain() in settings.ALLOWED_HOSTS:
+            return super().get_traceback_frames()
+        return {}
+
+
+    def _get_domain(self):
+        host = self.request._get_raw_host()
+        return host.split(':')[0]
