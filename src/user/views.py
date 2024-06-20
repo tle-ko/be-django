9b9132c6a3231613ab6dc9c5724218dd14cf5faf
Hepheir <hepheir@gmail.com>
2024-06-20 21:48:01 +0900

    feat(user.views): create `UserAPIView.ListCreate`

--- src/user/views.py
+++ src/user/views.py
@@ -3,3 +3,10 @@ from rest_framework.permissions import *
 
 from .models import *
 from .serializers import *
+
+
+class UserAPIView:
+    class ListCreate(ListCreateAPIView):
+        queryset = User.objects.all()
+        serializer_class = UserSerializer
+        permission_classes = [IsAdminUser]
