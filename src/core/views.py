699968cda2c3f877ed1cf1e2d7191904ff6c4e1a
Hepheir <hepheir@gmail.com>
2024-06-20 20:53:58 +0900

    feat(core.views): create `TagAPIView.ListCreate`

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,14 @@
 from rest_framework.generics import *
 from rest_framework.permissions import *
 
+from config.permissions import *
+
 from .models import *
 from .serializers import *
+
+
+class TagAPIView:
+    class ListCreate(ListCreateAPIView):
+        queryset = Tag.objects.all()
+        serializer_class = TagSerializer
+        permission_classes = [IsAdminUser|ReadOnly]
