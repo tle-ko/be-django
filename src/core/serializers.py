14e1502786b73b17d2fe044eb1ff0d37510dd821
Hepheir <hepheir@gmail.com>
2024-06-20 20:43:47 +0900

    feat(core.serializers): create `LanguageSerializer`

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -7,3 +7,9 @@ class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
+
+class LanguageSerializer(ModelSerializer):
+    class Meta:
+        model = Language
+        fields = '__all__'
