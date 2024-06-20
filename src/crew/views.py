dd6a8ca9e0b5aeb3dbb626cecc9a015b5224112b
Hepheir <hepheir@gmail.com>
2024-06-20 21:03:38 +0900

    feat(crew.views): create `CrewAPIView.ListCreate`

--- src/crew/views.py
+++ src/crew/views.py
@@ -1,5 +1,20 @@
 from rest_framework.generics import *
 from rest_framework.permissions import *
 
+from user.models import User
+
 from .models import *
 from .serializers import *
+
+
+class CrewAPIView:
+    class ListCreate(ListCreateAPIView):
+        queryset = Crew.objects.all()
+        serializer_class = CrewSerializer
+        permission_classes = [IsAuthenticated]
+
+        def perform_create(self, serializer):
+            serializer.save(captain=self._get_user())
+
+        def _get_user(self) -> User:
+            return User.objects.get(pk=self.request.user.pk)
