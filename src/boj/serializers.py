98eadcfc5152d2c256bf35d2e39e157c85587213
Hepheir <hepheir@gmail.com>
2024-06-21 01:02:56 +0900

    feat(boj.serializers): create `BOJUserSerializer`

--- src/boj/serializers.py
+++ src/boj/serializers.py
@@ -1,3 +1,18 @@
 from rest_framework.serializers import *
 
 from boj.models import *
+
+
+class BOJUserSerializer(ModelSerializer):
+    class Meta:
+        model = BOJUser
+        fields = [
+            'boj_id',
+            'level',
+            'is_verified',
+        ]
+        extra_kwargs = {
+            'boj_id': {'read_only': True},
+            'level': {'read_only': True},
+            'is_verified': {'read_only': True},
+        }
