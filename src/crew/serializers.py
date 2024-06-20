f0874f604b5da786bf1e7e155c8e19d8e341589e
Hepheir <hepheir@gmail.com>
2024-06-20 20:17:43 +0900

    feat(crew.serializers): create `CrewSerializer`

--- src/crew/serializers.py
+++ src/crew/serializers.py
@@ -1,3 +1,15 @@
 from rest_framework.serializers import *
 
+from core.serializers import LanguageSerializer
+from user.serializers import UserSerializer
+
 from .models import *
+
+
+class CrewSerializer(ModelSerializer):
+    captain = UserSerializer(read_only=True)
+    languages = LanguageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Crew
+        fields = '__all__'
