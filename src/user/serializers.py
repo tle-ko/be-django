5545bf8de04e7c02f0fb01ffd35319dc5b4a2f8f
Hepheir <hepheir@gmail.com>
2024-06-20 21:48:29 +0900

    feat(user.serializers): create `UserSerializer`

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework.serializers import *
 
 from .models import *
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'image',
+            'username',
+            'email',
+        ]
+
+    # TODO: BOJUser Serializer 연결
