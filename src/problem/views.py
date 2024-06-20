ed5892ad1d2ec362c141869d4f18696596ee167f
Hepheir <hepheir@gmail.com>
2024-06-20 21:42:01 +0900

    feat(problem.views): create `ProblemAPIView.ListCreate`

--- src/problem/views.py
+++ src/problem/views.py
@@ -3,3 +3,10 @@ from rest_framework.permissions import *
 
 from .models import *
 from .serializers import *
+
+
+class ProblemAPIView:
+    class ListCreate(ListCreateAPIView):
+        queryset = Problem.objects.all()
+        serializer_class = ProblemSerializer
+        permission_classes = [IsAuthenticated]
