853cd73530b7bae9b05583bd4a02d3e05c8bebb3
Hepheir <hepheir@gmail.com>
2024-06-20 23:19:48 +0900

    refactor(problem.serializers): update `ProblemSerializer`

--- src/problem/serializers.py
+++ src/problem/serializers.py
@@ -27,19 +27,23 @@ class ProblemAnalysisSerializer(ModelSerializer):
 class ProblemSerializer(ModelSerializer):
     user = UserSerializer(read_only=True)
     analysis = ProblemAnalysisSerializer(many=True, read_only=True)
-    time_limit = SerializerMethodField()
-    memory_limit = SerializerMethodField()
 
     class Meta:
         model = Problem
-        fields = '__all__'
+        fields = [
+            'id',
+            'analysis',
+            'title',
+            'link',
+            'description',
+            'input_description',
+            'output_description',
+            'memory_limit',
+            'time_limit',
+            'user',
+        ]
         extra_kwargs = {
-            'created_at': {'read_only': True},
-            'updated_at': {'read_only': True},
+            'id': {'read_only': True},
+            'analysis': {'read_only': True},
+            'user': {'read_only': True},
         }
-
-    def get_time_limit(self, obj: Problem) -> float:
-        return obj.time_limit/1000
-
-    def get_memory_limit(self, obj: Problem) -> int:
-        return round(obj.memory_limit/1024/1024)
