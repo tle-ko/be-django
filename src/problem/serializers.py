c2c6522c9e507a05124c9fa339b5b5db393bfb5e
Hepheir <hepheir@gmail.com>
2024-06-20 20:13:23 +0900

    feat(problem.serializers): `ProblemSerializer.time_limit`의 단위를 초 로 변경

--- src/problem/serializers.py
+++ src/problem/serializers.py
@@ -21,6 +21,7 @@ class ProblemAnalysisSerializer(ModelSerializer):
 class ProblemSerializer(ModelSerializer):
     user = UserSerializer(read_only=True)
     analysis = ProblemAnalysisSerializer(many=True, read_only=True)
+    time_limit = SerializerMethodField()
 
     class Meta:
         model = Problem
@@ -29,3 +30,6 @@ class ProblemSerializer(ModelSerializer):
             'created_at': {'read_only': True},
             'updated_at': {'read_only': True},
         }
+
+    def get_time_limit(self, obj: Problem) -> float:
+        return obj.time_limit/1000
