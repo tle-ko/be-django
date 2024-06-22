32c28c10a8dac35eb9a0c256583b1b99a2eeb0e4
Hepheir <hepheir@gmail.com>
2024-06-23 06:33:23 +0900

    chore(config.urls): make draft of overall API endpoints

--- src/config/urls.py
+++ src/config/urls.py
@@ -17,13 +17,75 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import (
+    include,
+    path,
+)
 
 
-API_VERSION_PREFIX = "api/v1"
+VIEW_PLACE_HOLDER = lambda request: NotImplemented
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include([
+        path("v1/", include([
+            path("account/", include([
+                path("signup", VIEW_PLACE_HOLDER), # TODO: 회원가입 기능 구현
+                path("signin", VIEW_PLACE_HOLDER), # TODO: 로그인 기능 구현
+                path("signout", VIEW_PLACE_HOLDER), # TODO: 로그아웃 기능 구현
+            ])),
+            path("user/", include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 사용자 목록 조회 기능 구현 (관리자용)
+                path("<int:id>/", include([
+                    path("", VIEW_PLACE_HOLDER), # TODO: 사용자 상세 조회+수정 기능 구현 (관리자용)
+                ])),
+            ])),
+            path("problem/", include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 전체 문제 목록 조회(관리자용) + 생성 기능 구현
+                path("my", VIEW_PLACE_HOLDER), # TODO: 내가 만든 문제 목록 조회 기능 구현
+                path("<int:id>/", include([
+                    path("", VIEW_PLACE_HOLDER), # TODO: 문제 상세 조회 기능 구현
+                    path("analysis", VIEW_PLACE_HOLDER), # TODO: 문제 분석 조회 기능 구현
+                ])),
+            ])),
+            path("crew/", include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 전체 크루 목록 조회(관리자용) + 생성 기능 구현
+                path("my", VIEW_PLACE_HOLDER), # TODO: 내가 속한 크루 목록 조회 기능 구현
+                path("recruiting", VIEW_PLACE_HOLDER), # TODO: 크루원을 모집 중인 크루 목록 조회 기능 구현
+                path("<int:id>/", include([
+                    path("", VIEW_PLACE_HOLDER), # TODO: 크루 상세 조회(공지사항, 크루원 목록, 해결한 문제들의 태그 분포, 이번 주 현황, 모집 시작/종료/옵션, ...)+수정 기능 구현
+                    path("activities", VIEW_PLACE_HOLDER), # TODO: 크루의 활동 회차 목록 조회 기능 구현
+                    path("problems", VIEW_PLACE_HOLDER), # TODO: 크루에 속한 문제 목록 조회 기능 구현
+                    path("pending", VIEW_PLACE_HOLDER), # TODO: 크루 가입 대기자 목록 조회 기능 구현
+                ])),
+            ])),
+            path("activity/", include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 크루 활동 회차 목록 조회 + 추가(방장만) 기능 구현
+                path("<int:id>/", include([
+                    path("", VIEW_PLACE_HOLDER), # TODO: 크루 활동 회차 상세 조회 기능 구현
+                ])),
+            ])),
+            path("submission/", include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 크루 활동 문제에 대한 풀이 제출 + 목록 조회 기능 구현
+                path("<int:id>/", include([
+                    path("", VIEW_PLACE_HOLDER), # TODO: 제출 상세 조회+수정 기능 구현
+                ])),
+            ])),
+            path("comment/",include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 코멘트 목록 조회(관리자용) + 생성 기능 구현
+                path("<int:id>/", include([
+                    path("", VIEW_PLACE_HOLDER), # TODO: 코멘트 상세 조회+수정+삭제 기능 구현
+                ])),
+            ])),
+            path("tag/", include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 전체 태그 목록 조회(관리자용) + 생성 기능 구현
+            ])),
+            path("language/", include([
+                path("", VIEW_PLACE_HOLDER), # TODO: 전체 언어 목록 조회(관리자용) + 생성 기능 구현
+            ])),
+        ])),
+    ])),
 ]
 
 # Static files
