bd412101751b8318620f24f2941311ebd639dfb9
Hepheir <hepheir@gmail.com>
2024-06-23 07:03:41 +0900

    feat(core.views): `page_size=250` 으로 Pagination 활성화 (`TagAPIView`, `LanguageAPIView`)

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import *
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import *
 
 from config.permissions import *
@@ -7,11 +8,17 @@ from .models import *
 from .serializers import *
 
 
+class _PageNumberPagination(PageNumberPagination):
+    page_size = 250
+    page_size_query_param = 'page_size'
+
+
 class TagAPIView:
     class ListCreate(ListCreateAPIView):
         queryset = Tag.objects.all()
         serializer_class = TagSerializer
         permission_classes = [IsAdminUser|ReadOnly]
+        pagination_class = _PageNumberPagination
 
 
 class LanguageAPIView:
@@ -19,3 +26,4 @@ class LanguageAPIView:
         queryset = Language.objects.all()
         serializer_class = LanguageSerializer
         permission_classes = [IsAdminUser|ReadOnly]
+        pagination_class = _PageNumberPagination
