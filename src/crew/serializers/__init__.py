5fd40dd54b74569d8e7eabb0cb16118b4b4e6954
Hepheir <hepheir@gmail.com>
2024-06-26 14:05:09 +0900

    feat(crew.serializers): add `CrewSerializer` and `CrewMemberSerializer` serializers module

---
+++ src/crew/serializers/__init__.py
@@ -0,0 +1,8 @@
+from crew.serializers.crew import CrewSerializer
+from crew.serializers.crew_member import CrewMemberSerializer
+
+
+__all__ = [
+    'CrewSerializer',
+    'CrewMemberSerializer',
+]
