ac470b811f3d89aee7c2db643d5bcb330bac3703
Hepheir <hepheir@gmail.com>
2024-06-30 16:32:05 +0900

    feat(crew.serializers): create `CrewDetailSerializer`

--- src/crew/serializers/__init__.py
+++ src/crew/serializers/__init__.py
@@ -1,8 +1,12 @@
-from crew.serializers.crew import CrewSerializer
+from crew.serializers.crew import (
+    CrewSerializer,
+    CrewDetailSerializer,
+)
 from crew.serializers.crew_member import CrewMemberSerializer
 
 
 __all__ = [
     'CrewSerializer',
+    'CrewDetailSerializer',
     'CrewMemberSerializer',
 ]
--- src/crew/serializers/crew.py
+++ src/crew/serializers/crew.py
@@ -8,6 +8,7 @@ from crew.serializers.mixins import (
     TagsMixin,
 )
 from crew.models import Crew
+from crew.serializers.crew_activity import CrewActivitySerializer
 
 
 class CrewSerializer(ModelSerializer, MembersMixin, TagsMixin):
@@ -23,5 +24,25 @@ class CrewSerializer(ModelSerializer, MembersMixin, TagsMixin):
             'name',
             'members',
             'tags',
+            'is_recruiting',
+        ]
+
+
+class CrewDetailSerializer(ModelSerializer, MembersMixin, TagsMixin):
+    """크루에 대한 상세 데이터를 다룬다."""
+
+    members = SerializerMethodField()
+    tags = SerializerMethodField()
+    activities = CrewActivitySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Crew
+        fields = [
+            'emoji',
+            'name',
+            'members',
+            'tags',
+            'activities',
+            'is_active',
             'is_recruiting',
         ]
