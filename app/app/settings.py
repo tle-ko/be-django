f54ad7d632bb89f4cce9df9f57db61a86afb2c90
Hepheir <hepheir@gmail.com>
2024-07-17 03:21:30 +0900

    refactor(app.settings): fix parent path

--- app/app/settings.py
+++ app/app/settings.py
@@ -18,7 +18,7 @@ GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY", None)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
