d26ff60a6b0ec0f2731ca94ecd301ac7983980cf
Hepheir <hepheir@gmail.com>
2024-07-17 18:55:24 +0900

    refactor(tools): delete tools/*

--- tools/runserver.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-REPOSITORY=$(dirname $(dirname $0))
-
-cd $REPOSITORY/src
-python manage.py runserver 0.0.0.0:80 --insecure > $REPOSITORY/.log 2>&1 &
--- tools/setup.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-REPOSITORY=$(dirname $(dirname $0))
-
-cd $REPOSITORY/src
-python manage.py shell < $REPOSITORY/tools/db/setup_db.py
