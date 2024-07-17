073d2e06063dc16015d9035215262e92e755699a
Hepheir <hepheir@gmail.com>
2024-07-17 16:51:04 +0900

    feat(tle.backends): create `UserAuthBackend`

--- app/app/settings.py
+++ app/app/settings.py
@@ -112,6 +112,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'tle.User'
 
+AUTHENTICATION_BACKENDS = [
+    'tle.backends.UserAuthBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
---
+++ app/tle/backends.py
@@ -0,0 +1,22 @@
+import logging
+
+from django.contrib.auth.backends import ModelBackend
+from django.http import HttpRequest
+
+from tle.models import User
+
+
+logger = logging.getLogger(__name__)
+
+
+class UserAuthBackend(ModelBackend):
+    def authenticate(self, request: HttpRequest, username=None, password=None, **kwargs):
+        """username 필드지만 email로 인증하도록 오버라이드 되어있음."""
+        try:
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            return None
+        else:
+            if user.check_password(password):
+                return user
+        return None
