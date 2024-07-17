b78c44554f2ed94e310e31a3470584f06b55b96a
Hepheir <hepheir@gmail.com>
2024-07-17 18:02:54 +0900

    feat(tle.views): create urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,9 +19,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+import tle.views.urls
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/", include(tle.views.urls.urlpatterns)),
 ]
 
 # Static files
---
+++ app/tle/views/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+
+from tle.views.viewsets import *
+
+
+urlpatterns = [
+]
