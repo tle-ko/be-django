85c8b2a33c426a7f3bbb8e7c5fec441b5e2ac5db
Hepheir <hepheir@gmail.com>
2024-07-18 11:43:36 +0900

    feat(tle.models): add `get_name()` to `ProblemDifficulty`

--- app/tle/models/problem_difficulty.py
+++ app/tle/models/problem_difficulty.py
@@ -1,7 +1,29 @@
 from django.db import models
 
 
+NAMES = {
+    'ko': {
+        1: '쉬움',
+        2: '보통',
+        3: '어려움',
+    },
+    'en': {
+        1: 'EASY',
+        2: 'NORMAL',
+        3: 'HARD',
+    },
+}
+
+
 class ProblemDifficulty(models.IntegerChoices):
     EASY = 1, '쉬움'
     NORMAL = 2, '보통'
     HARD = 3, '어려움'
+
+    def get_name(self, lang='ko') -> str:
+        if lang not in NAMES:
+            raise ValueError(
+                f'Invalid language: {lang}, ',
+                f'choose from {NAMES.keys()}'
+            )
+        return NAMES[lang][self.value]
