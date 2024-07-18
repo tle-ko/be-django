729a116c35019f452adcf5be1b1b202c2ab74681
Hepheir <hepheir@gmail.com>
2024-07-18 15:26:56 +0900

    refactor(tle.views): REST API 엔드포인트를 `auth`, `user` 두 개로 분리

--- app/tle/views/urls.py
+++ app/tle/views/urls.py
@@ -5,16 +5,27 @@ from tle.views.viewsets import *
 
 urlpatterns = [
     path("auth/", include([
-        path("signin", UserViewSet.as_view({"post": "sign_in"})),
-        path("signup", UserViewSet.as_view({"post": "sign_up"})),
-        path("signout", UserViewSet.as_view({"get": "sign_out"})),
+        path("signin", AuthViewSet.as_view({"post": "sign_in"})),
+        path("signup", AuthViewSet.as_view({"post": "sign_up"})),
+        path("signout", AuthViewSet.as_view({"get": "sign_out"})),
     ])),
     path("user/current", UserViewSet.as_view({"get": "current"})),
+    path("user/", include([
+        path("search", UserViewSet.as_view({"get": "list"})),
+        path("<int:id>/", include([
+            path("profile", UserViewSet.as_view({
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            })),
+        ])),
+    ])),
     path("problem/", include([
         path("", ProblemViewSet.as_view({"post": "create"})),
         path("search", ProblemViewSet.as_view({"get": "list"})),
         path("<int:id>/", include([
-            path("detail", ProblemViewSet.as_view({
+            path("description", ProblemViewSet.as_view({
                 "get": "retrieve",
                 "put": "update",
                 "patch": "partial_update",
--- app/tle/views/viewsets/__init__.py
+++ app/tle/views/viewsets/__init__.py
@@ -1,2 +1,3 @@
+from tle.views.viewsets.auth_viewset import AuthViewSet
 from tle.views.viewsets.user_viewset import UserViewSet
 from tle.views.viewsets.problem_viewset import ProblemViewSet
