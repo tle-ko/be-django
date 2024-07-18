94e3040a56512911ef5e28a900adeadffb36ec5d
Hepheir <hepheir@gmail.com>
2024-07-18 12:33:08 +0900

    feat(tle.models): `ProblemDifficulty`에 `UNDER ANALYASIS` 선택지를 추가

--- app/tle/models/problem_difficulty.py
+++ app/tle/models/problem_difficulty.py
@@ -3,11 +3,13 @@ from django.db import models
 
 NAMES = {
     'ko': {
+        0: '분석 중',
         1: '쉬움',
         2: '보통',
         3: '어려움',
     },
     'en': {
+        0: 'UNDER ANALYSIS',
         1: 'EASY',
         2: 'NORMAL',
         3: 'HARD',
@@ -16,14 +18,18 @@ NAMES = {
 
 
 class ProblemDifficulty(models.IntegerChoices):
-    EASY = 1, '쉬움'
-    NORMAL = 2, '보통'
-    HARD = 3, '어려움'
-
-    def get_name(self, lang='ko') -> str:
+    @classmethod
+    def get_name(cls, value: int, lang='ko') -> str:
         if lang not in NAMES:
             raise ValueError(
                 f'Invalid language: {lang}, ',
                 f'choose from {NAMES.keys()}'
             )
-        return NAMES[lang][self.value]
+        return NAMES[lang][value]
+
+    EASY = 1, '쉬움'
+    NORMAL = 2, '보통'
+    HARD = 3, '어려움'
+
+    def get_name(self, value: int, lang='ko') -> str:
+        return ProblemDifficulty.get_name(value, lang)
