55ce1683e004ed2ddb9da98e5cbf584a4cfc6305
Hepheir <hepheir@gmail.com>
2024-07-18 14:51:00 +0900

    refactor(tle.views): redesign urls under `api/v1/account` (renamed `account` to `auth`)

--- app/tle/views/urls.py
+++ app/tle/views/urls.py
@@ -4,11 +4,10 @@ from tle.views.viewsets import *
 
 
 urlpatterns = [
-    path("account/", include([
+    path("auth/", include([
         path("signin", UserViewSet.as_view({"post": "sign_in"})),
         path("signup", UserViewSet.as_view({"post": "sign_up"})),
         path("signout", UserViewSet.as_view({"get": "sign_out"})),
-        path("current", UserViewSet.as_view({"get": "current"})),
     ])),
     path("problem/", include([
         path("", ProblemViewSet.as_view({"post": "create"})),
