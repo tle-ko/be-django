09e3b2ab80986eaf88a332d2423f2310af5f0fa5
Hepheir <hepheir@gmail.com>
2024-07-19 22:41:22 +0900

    feat(tle.serializers): add `is_member`, `is_joinable` fields

--- app/tle/serializers/crew_recruiting.py
+++ app/tle/serializers/crew_recruiting.py
@@ -1,24 +1,32 @@
-import typing
-
 from rest_framework.serializers import *
 
-from tle.models import Crew, User, UserSolvedTier
+from tle.models import Crew
+from tle.serializers.mixins import CurrentUserMixin
 
 
-class CrewRecruitingSerializer(ModelSerializer):
+class CrewRecruitingSerializer(ModelSerializer, CurrentUserMixin):
+    is_joinable = SerializerMethodField()
+    is_member = SerializerMethodField()
     members = SerializerMethodField()
     tags = SerializerMethodField()
-    is_joinable = SerializerMethodField()
 
     class Meta:
         model = Crew
         fields = [
             'name',
             'emoji',
+            'is_recruiting',
+            'is_joinable',
+            'is_member',
             'members',
             'tags',
-            'is_joinable',
         ]
+
+    def get_is_joinable(self, obj: Crew):
+        return obj.is_recruiting
+
+    def get_is_member(self, obj: Crew):
+        return obj.members.filter(user=self.current_user()).exists()
 
     def get_members(self, obj: Crew):
         return {
@@ -27,48 +35,8 @@ class CrewRecruitingSerializer(ModelSerializer):
         }
 
     def get_tags(self, obj: Crew):
-        tags = [
-            *self._get_language_tags(obj),
-            *self._get_tier_tags(obj),
-            *self._get_custom_tags(obj),
-        ]
+        tags = obj.get_tags()
         return {
             'count': len(tags),
-            'items': tags,
-        }
-
-    def get_is_joinable(self, obj: Crew):
-        user = self._get_user()
-        assert user.is_authenticated
-        return obj.is_joinable(user)
-
-    def _get_user(self) -> User:
-        return self.context['request'].user
-
-    def _get_language_tags(self, obj: Crew):
-        return [
-            self._tag_item(lang.key, lang.name)
-            for lang in obj.submittable_languages.all()
-        ]
-
-    def _get_tier_tags(self, obj: Crew):
-        tags = []
-        if obj.min_boj_tier is not None:
-            name = f'{UserSolvedTier(obj.min_boj_tier).label} 이상'
-            tags.append(self._tag_item(None, name))
-        if obj.max_boj_tier is not None:
-            name = f'{UserSolvedTier(obj.max_boj_tier).label} 이하'
-            tags.append(self._tag_item(None, name))
-        if obj.min_boj_tier is None and obj.max_boj_tier is None:
-            name = f'티어 무관'
-            tags.append(self._tag_item(None, name))
-        return tags
-
-    def _get_custom_tags(self, obj: Crew):
-        return [self._tag_item(None, tag) for tag in obj.custom_tags]
-
-    def _tag_item(self, key: typing.Optional[str], name: str):
-        return {
-            'key': key,
-            'name': name,
+            'items': [{'key': tag.key, 'name': tag.name} for tag in tags],
         }
