57dcd4b61bf6d342ed66af94cd84d23c6e2c90d6
Hepheir <hepheir@gmail.com>
2024-07-20 05:33:13 +0900

    refactor(tle.views): add `__all__`

--- app/tle/views/viewsets/__init__.py
+++ app/tle/views/viewsets/__init__.py
@@ -2,3 +2,11 @@ from tle.views.viewsets.auth_viewset import AuthViewSet
 from tle.views.viewsets.user_viewset import UserViewSet
 from tle.views.viewsets.problem_viewset import ProblemViewSet
 from tle.views.viewsets.crew_viewset import CrewViewSet
+
+
+__all__ = (
+    'AuthViewSet',
+    'UserViewSet',
+    'ProblemViewSet',
+    'CrewViewSet',
+)
