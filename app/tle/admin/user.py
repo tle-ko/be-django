214f85552d28d47c91372fcdd51464b478c9a6dc
Hepheir <hepheir@gmail.com>
2024-07-21 02:48:02 +0900

    fix(tle.admin): `User` 객체를 수정할 수 없던 오류 정정 (필드명 불일치 오류)

--- app/tle/admin/user.py
+++ app/tle/admin/user.py
@@ -7,11 +7,17 @@ from tle.models import User
 @admin.register(User)
 class UserModelAdmin(UserAdmin):
     fieldsets = [
-        *UserAdmin.fieldsets,
         (None, {'fields': [
-            'profile_image',
-            'boj_username',
-            'boj_tier',
-            'boj_tier_updated_at',
+            User.field_name.EMAIL,
+            User.field_name.USERNAME,
+            User.field_name.PASSWORD,
+            User.field_name.PROFILE_IMAGE,
+            User.field_name.BOJ_USERNAME,
+            User.field_name.BOJ_LEVEL,
+            User.field_name.BOJ_LEVEL_UPDATED_AT,
+            User.field_name.IS_ACTIVE,
+            User.field_name.IS_STAFF,
+            User.field_name.IS_SUPERUSER,
+            User.field_name.CREATED_AT,
         ]}),
     ]
