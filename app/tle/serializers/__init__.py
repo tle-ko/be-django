0ae2b50ccb132988ca6a9bcbe8fa660afbeb070a
Hepheir <hepheir@gmail.com>
2024-07-25 13:24:23 +0900

    feat(tle.serializers): add `CrewDetailSerializer`

--- app/tle/serializers/__init__.py
+++ app/tle/serializers/__init__.py
@@ -6,6 +6,7 @@ from tle.serializers.problem_detail import ProblemDetailSerializer
 from tle.serializers.problem_minimal import ProblemMinimalSerializer
 from tle.serializers.problem_tag import ProblemTagSerializer
 
+from tle.serializers.crew_detail import CrewDetailSerializer
 from tle.serializers.crew_member import CrewMemberSerializer
 from tle.serializers.crew_recruiting import CrewRecruitingSerializer
 from tle.serializers.crew_joined import CrewJoinedSerializer
@@ -26,6 +27,7 @@ __all__ = (
     'ProblemMinimalSerializer',
     'ProblemTagSerializer',
 
+    'CrewDetailSerializer',
     'CrewMemberSerializer',
     'CrewRecruitingSerializer',
     'CrewJoinedSerializer',
