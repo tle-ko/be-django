c24f759386096e4aaec68d8fa9998b919771b494
Hepheir <hepheir@gmail.com>
2024-07-30 04:47:44 +0900

    feat(users.models): create `UserEmailVerification` model #3

--- app/users/models/__init__.py
+++ app/users/models/__init__.py
@@ -1,12 +1,12 @@
-from users.models.user import User
-from users.models.user_manager import UserManager
-
 from users.models.choices import UserBojLevelChoices
+from users.models.user import User
+from users.models.user_email_verification import UserEmailVerification
+from users.models.user_manager import UserManager
 
 
 __all__ = (
     'User',
+    'UserEmailVerification',
     'UserManager',
-
     'UserBojLevelChoices',
 )
---
+++ app/users/models/user_email_verification.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+from datetime import timedelta
+from hashlib import sha256
+from random import randint
+
+from django.db import models
+from django.utils import timezone
+
+
+N_CODES = 6
+
+A, Z = ord('A'), ord('Z')
+
+
+def default_verification_code_factory() -> str:
+    def code(length=N_CODES):
+        for _ in range(length):
+            yield chr(randint(A, Z))
+    return ''.join(code())
+
+
+def default_verification_token_factory() -> str:
+    return sha256(default_verification_code_factory()).hexdigest()
+
+
+def default_expires_at_factory():
+    return timezone.now() + timedelta(minutes=5)
+
+
+class UserEmailVerification(models.Model):
+    email = models.EmailField(
+        help_text='이메일 주소',
+        primary_key=True,
+    )
+    verification_code = models.CharField(
+        help_text='인증 코드',
+        max_length=6,
+        default=default_verification_code_factory,
+    )
+    verification_token = models.TextField(
+        help_text='인증 토큰',
+        default=default_verification_token_factory,
+    )
+    expires_at = models.DateTimeField(default=default_expires_at_factory)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class field_name:
+        EMAIL = 'email'
+        VERIFICATION_CODE = 'verification_code'
+        VERIFICATION_TOKEN = 'verification_token'
+        EXPIRES_AT = 'expires_at'
+        CREATED_AT = 'created_at'
+
+    def is_expired(self) -> bool:
+        return self.expires_at < timezone.now()
