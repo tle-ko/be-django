de8000fb44c5cf891d184ba7dbf6ce44e403767d
Hepheir <hepheir@gmail.com>
2024-08-07 12:59:16 +0900

    refactor: url 임포트 방식 변경

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,55 +1,30 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from users.views import *
-from problems.views import ProblemCreate, ProblemDetail, ProblemSearch
-from crews.views import CrewCreate, CrewDetail, CrewRecruiting, CrewJoined
+import crews.views
+import problems.views
+import users.views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include([
-        path("auth/", include([
-            path("signin", SignIn.as_view()),
-            path("signup", SignUp.as_view()),
-            path("signout", SignOut.as_view()),
-            path("verification", EmailVerification.as_view()),
-        ])),
-        path("users/current", CurrentUser.as_view()),
-        path("problems/", include([
-            path("", ProblemCreate.as_view()),
-            path("search", ProblemSearch.as_view()),
-            path("<int:id>/detail", ProblemDetail.as_view()),
-        ])),
-        path("crews/", include([
-            path("", CrewCreate.as_view()),
-            path("recruiting", CrewRecruiting.as_view()),
-            path("my", CrewJoined.as_view()),
-            path("<int:id>/detail", CrewDetail.as_view()),
-        ])),
+        path("auth/signin", users.views.SignInAPIView.as_view()),
+        path("auth/signup", users.views.SignUpAPIView.as_view()),
+        path("auth/signout", users.views.SignOutAPIView.as_view()),
+        path("auth/verification/code", users.views.EmailVerificationCodeAPIView.as_view()),
+        path("auth/verification/token", users.views.EmailVerificationTokenAPIView.as_view()),
+        path("crews/", crews.views.CrewCreate.as_view()),
+        path("crews/recruiting", crews.views.CrewRecruiting.as_view()),
+        path("crews/my", crews.views.CrewJoined.as_view()),
+        path("crews/<int:id>/detail", crews.views.CrewDetail.as_view()),
+        path("problems/", problems.views.ProblemCreate.as_view()),
+        path("problems/search", problems.views.ProblemSearch.as_view()),
+        path("problems/<int:id>/detail", problems.views.ProblemDetail.as_view()),
+        path("users/current", users.views.CurrentUserAPIView.as_view()),
     ])),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
-
-# Static files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Media files
-# TODO: 미디어 파일은 S3 같은 외부 의존성으로 변경하기
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
