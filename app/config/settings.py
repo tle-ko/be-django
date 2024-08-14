7f66f0036f174a535a7357f4a174983c0cdc8e14
Hepheir <hepheir@gmail.com>
2024-08-14 14:28:17 +0900

    feat(config): jwt 로그인 외에도 session 로그인을 지원

--- app/config/settings.py
+++ app/config/settings.py
@@ -136,6 +136,8 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    	'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
