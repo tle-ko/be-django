acf52990c4f6b2e35b835c64ca6e6878923271cc
Hepheir <hepheir@gmail.com>
2024-08-14 14:27:49 +0900

    fix(users): 2중 이메일 인증 문제로 인한 회원가입 실패 오류 해결

--- app/users/services/authentication.py
+++ app/users/services/authentication.py
@@ -16,13 +16,6 @@ def sign_up(email: str, username: str, password: str, **extra_fields) -> User:
     이메일 인증 토큰이 필요합니다.
     인증에 실패할 경우 ValidationError를 발생시킵니다."""
     user_manager: UserManager = User.objects
-    verification_token = extra_fields.pop('verification_token', None)
-    # 이메일 주소 인증
-    if not UserEmailVerification.objects.filter(**{
-        UserEmailVerification.field_name.EMAIL: email,
-        UserEmailVerification.field_name.VERIFICATION_TOKEN: verification_token
-    }).exists():
-        raise ValidationError('Email is not verified.')
     return user_manager.create_user(email, username, password, **extra_fields)
 
 def sign_in(request: Request, email: str, password: str) -> User:
