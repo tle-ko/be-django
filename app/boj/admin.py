9c0800b5eed5e0718fc9ab07686a8bf23d2dafd6
Hepheir <hepheir@gmail.com>
2024-08-28 23:06:58 +0900

    refactor(boj.services): signals.py에서 시그널을 정의하지 않으므로 services로 모듈명 변경

--- app/boj/admin.py
+++ app/boj/admin.py
@@ -2,33 +2,34 @@ from django.contrib import admin
 from django.db.models import QuerySet
 from django.http.request import HttpRequest
 
-from boj import models
-from boj.signals import update_boj_user_data
+from boj.models import BOJUser
+from boj.models import BOJUserSnapshot
+from boj.services import update_boj_user_data
 
 
-@admin.register(models.BOJUser)
+@admin.register(BOJUser)
 class BOJUserModelAdmin(admin.ModelAdmin):
     list_display = [
-        models.BOJUser.field_name.USERNAME,
-        models.BOJUser.field_name.LEVEL,
-        models.BOJUser.field_name.RATING,
-        models.BOJUser.field_name.UPDATED_AT,
+        BOJUser.field_name.USERNAME,
+        BOJUser.field_name.LEVEL,
+        BOJUser.field_name.RATING,
+        BOJUser.field_name.UPDATED_AT,
     ]
     actions = [
         'update',
     ]
 
     @admin.action(description="Update selected BOJ user data. (via solved.ac API)")
-    def update(self, request: HttpRequest, queryset: QuerySet[models.BOJUser]):
+    def update(self, request: HttpRequest, queryset: QuerySet[BOJUser]):
         for obj in queryset:
             update_boj_user_data(obj.username)
 
 
-@admin.register(models.BOJUserSnapshot)
+@admin.register(BOJUserSnapshot)
 class BOJUserSnapshotModelAdmin(admin.ModelAdmin):
     list_display = [
-        models.BOJUserSnapshot.field_name.USER,
-        models.BOJUserSnapshot.field_name.LEVEL,
-        models.BOJUserSnapshot.field_name.RATING,
-        models.BOJUserSnapshot.field_name.CREATED_AT,
+        BOJUserSnapshot.field_name.USER,
+        BOJUserSnapshot.field_name.LEVEL,
+        BOJUserSnapshot.field_name.RATING,
+        BOJUserSnapshot.field_name.CREATED_AT,
     ]
