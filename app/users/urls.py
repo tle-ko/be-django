02368a80e8888de639974b4d04d203605935800b
Hepheir <hepheir@gmail.com>
2024-08-28 23:46:14 +0900

    refactor(users.urls): users 모듈 url들 중 일부 path들을 그룹화

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,14 +1,19 @@
+from django.urls import include
 from django.urls import path
 
 import users.views
 
 
 urlpatterns = [
-    path("auth/signin", users.views.SignInAPIView.as_view()),
-    path("auth/signup", users.views.SignUpAPIView.as_view()),
-    path("auth/signout", users.views.SignOutAPIView.as_view()),
-    path("auth/username/check", users.views.UsernameCheckAPIView.as_view()),
-    path("auth/email/check", users.views.EmailCheckAPIView.as_view()),
-    path("auth/email/verify", users.views.EmailVerifyAPIView.as_view()),
-    path("user/manage", users.views.CurrentUserRetrieveUpdateAPIView.as_view()),
+    path("auth", include([
+        path("/signin", users.views.SignInAPIView.as_view()),
+        path("/signup", users.views.SignUpAPIView.as_view()),
+        path("/signout", users.views.SignOutAPIView.as_view()),
+        path("/username/check", users.views.UsernameCheckAPIView.as_view()),
+        path("/email/check", users.views.EmailCheckAPIView.as_view()),
+        path("/email/verify", users.views.EmailVerifyAPIView.as_view()),
+    ])),
+    path("user", include([
+        path("/manage", users.views.CurrentUserRetrieveUpdateAPIView.as_view()),
+    ])),
 ]
