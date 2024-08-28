04dfd598b9c6a147b4c8c105517a2f45e6bc3fa2
Hepheir <hepheir@gmail.com>
2024-08-28 23:37:02 +0900

    refactor(users.admins): 사용자가 소속된 크루 정보를 보여주는 열 제거 (의존성을 줄이기 위해)

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from users.models import User, UserEmailVerification
-from crews.models import CrewMember
+from users.models import User
+from users.models import UserEmailVerification
 
 
 admin.site.register([
@@ -17,20 +17,8 @@ class UserAdmin(BaseUserAdmin):
         User.field_name.USERNAME,
         User.field_name.EMAIL,
         User.field_name.BOJ_USERNAME,
-        'get_crews',
         User.field_name.IS_ACTIVE,
         User.field_name.IS_STAFF,
         User.field_name.IS_SUPERUSER,
         User.field_name.CREATED_AT,
     ]
-
-    @admin.display(description='captains / members')
-    def get_crews(self, user: User) -> str:
-        n_captains = CrewMember.objects.filter(**{
-            CrewMember.field_name.USER: user,
-            CrewMember.field_name.IS_CAPTAIN: True,
-        }).count()
-        n_members = CrewMember.objects.filter(**{
-            CrewMember.field_name.USER: user,
-        }).count()
-        return f'{n_captains} / {n_members}'
