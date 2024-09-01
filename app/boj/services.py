a9f38eb858148b42274c4fe3738fbf7c25abbb19
Hepheir <hepheir@gmail.com>
2024-09-01 10:39:02 +0900

    fix(boj.services): 없는 사용자명에 대한 예외처리 추가

--- app/boj/services.py
+++ app/boj/services.py
@@ -1,9 +1,11 @@
+from json import JSONDecodeError
 from logging import getLogger
 
 from background_task import background
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
+from rest_framework import status
 import requests
 
 from boj.models import BOJUser
@@ -35,13 +37,20 @@ def _update_boj_user_data(username: str):
     assert username.strip().isidentifier()
     instance = BOJUser.objects.get_by_username(username)
     url = f'https://solved.ac/api/v3/user/show?handle={username}'
-    data = requests.get(url).json()
-    try:
-        instance.level = data['tier']
-        instance.rating = data['rating']
-        instance.updated_at = timezone.now()
-        instance.save()
-        BOJUserSnapshot.objects.create_snapshot_of(instance)
-    except AssertionError:
-        # Solved.ac API 관련 문제일 가능성이 높다.
-        logger.warning(f'"{url}"로 부터 데이터를 파싱해오는 것에 실패했습니다.')
+    res = requests.get(url)
+    if res.status_code == status.HTTP_404_NOT_FOUND:
+        logger.info(f'사용자 명이 "{username}"인 사용자가 존재하지 않습니다.')
+    else:
+        try:
+            data = res.json()
+            instance.level = data['tier']
+            instance.rating = data['rating']
+            instance.updated_at = timezone.now()
+            instance.save()
+            BOJUserSnapshot.objects.create_snapshot_of(instance)
+        except AssertionError:
+            # Solved.ac API 관련 문제일 가능성이 높다.
+            logger.warning(f'"{url}"로 부터 데이터를 파싱해오는 것에 실패했습니다.')
+        except JSONDecodeError:
+            logger.warning(f'"{url}"로 부터 데이터를 파싱해오는 것에 실패했습니다.')
+            logger.error(f'받은 데이터: "{res.content}"')
