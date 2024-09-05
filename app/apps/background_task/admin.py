396329eb375bf77664492d0d6b4a47549abbab05
Hepheir <hepheir@gmail.com>
2024-09-05 12:05:39 +0900

    feat(apps.background_task.admin): reschedule now 명령 추가

--- app/apps/background_task/admin.py
+++ app/apps/background_task/admin.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from django.contrib import admin
 from apps.background_task.models import Task
 from apps.background_task.models import CompletedTask
@@ -16,11 +18,16 @@ def dec_priority(modeladmin, request, queryset):
         obj.save()
 dec_priority.short_description = "priority -= 1"
 
+def reschedule_now(modeladmin, request, queryset):
+    for obj in queryset:
+        obj.run_at = datetime.now()
+        obj.save()
+
 class TaskAdmin(admin.ModelAdmin):
     display_filter = ['task_name']
     search_fields = ['task_name', 'task_params', ]
     list_display = ['task_name', 'task_params', 'run_at', 'priority', 'attempts', 'has_error', 'locked_by', 'locked_by_pid_running', ]
-    actions = [inc_priority, dec_priority]
+    actions = [inc_priority, dec_priority, reschedule_now]
 
 class CompletedTaskAdmin(admin.ModelAdmin):
     display_filter = ['task_name']
