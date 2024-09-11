31d9ae0247ca698129d5019b55a995d124b78ac8
Hepheir <hepheir@gmail.com>
2024-09-11 20:59:20 +0900

    refactor(apps.submissions): 사용되지 않는 코드 제거

--- app/apps/submissions/serializers.py
+++ app/apps/submissions/serializers.py
@@ -1,17 +1,1 @@
 from rest_framework import serializers
-
-from apps.submissions.models import Submission
-
-
-class SubmissionSerializer(serializers.ModelField):
-    class Meta:
-        model = Submission
-        fields = [
-            'id',
-            Submission.field_name.USER,
-            Submission.field_name.CODE,
-            Submission.field_name.LANGUAGE,
-            Submission.field_name.IS_CORRECT,
-            Submission.field_name.IS_HELP_NEEDED,
-            Submission.field_name.CREATED_AT,
-        ]
