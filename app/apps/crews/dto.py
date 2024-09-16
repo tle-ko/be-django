eeb2de1708ab90e0b0ecfe245a53ddafba5db3bf
Hepheir <hepheir@gmail.com>
2024-09-17 03:43:08 +0900

    feat(apps.crews.dto): add more DTO

--- app/apps/crews/dto.py
+++ app/apps/crews/dto.py
@@ -1,6 +1,25 @@
 from dataclasses import dataclass
+from datetime import datetime
+from typing import List
 
 from apps.crews.enums import CrewTagType
+
+
+@dataclass
+class CrewDTO:
+    crew_id: int
+    id: int  # TODO: deprecate
+    name: str
+    icon: str
+    is_active: bool
+    is_captain: bool
+
+
+@dataclass
+class CrewMemberDTO:
+    user_id: int
+    username: str
+    is_captain: bool
 
 
 @dataclass
@@ -8,3 +27,19 @@ class CrewTagDTO:
     key: str
     name: str
     type: CrewTagType
+
+
+@dataclass
+class CrewActivityDTO:
+    activity_id: int
+    name: str
+    date_start_at: datetime
+    date_end_at: datetime
+
+
+@dataclass
+class CrewDashboardDTO(CrewDTO):
+    notice: str
+    tags: List[CrewTagDTO]
+    members: List[CrewMemberDTO]
+    activities: List[CrewActivityDTO]
