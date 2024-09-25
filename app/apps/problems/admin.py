0a61ac41904ea29443f374f8d082bef3f6a5e780
Hepheir <hepheir@gmail.com>
2024-09-25 20:39:31 +0900

    fix(problems): fix problem database viewer

--- app/apps/problems/admin.py
+++ app/apps/problems/admin.py
@@ -2,40 +2,39 @@ from django.contrib import admin
 from django.db.models import QuerySet
 
 from apps.boj.proxy import BOJTag
-from apps.llms import analyze
-from apps.llms import schedule_analyze
 from users.models import User
 
-from . import proxy
+from . import analyzer
+from . import models
 
 
-@admin.register(proxy.Problem)
+@admin.register(models.ProblemDAO)
 class ProblemModelAdmin(admin.ModelAdmin):
     list_display = [
-        proxy.Problem.field_name.TITLE,
-        proxy.Problem.field_name.CREATED_BY,
-        proxy.Problem.field_name.CREATED_AT,
-        proxy.Problem.field_name.UPDATED_AT,
+        models.ProblemDAO.field_name.TITLE,
+        models.ProblemDAO.field_name.CREATED_BY,
+        models.ProblemDAO.field_name.CREATED_AT,
+        models.ProblemDAO.field_name.UPDATED_AT,
     ]
     search_fields = [
-        proxy.Problem.field_name.TITLE,
-        proxy.Problem.field_name.CREATED_BY+'__'+User.field_name.USERNAME,
+        models.ProblemDAO.field_name.TITLE,
+        models.ProblemDAO.field_name.CREATED_BY+'__'+User.field_name.USERNAME,
     ]
-    ordering = ['-'+proxy.Problem.field_name.CREATED_AT]
+    ordering = ['-'+models.ProblemDAO.field_name.CREATED_AT]
     actions = [
         'action_schedule_analyze',
         'action_copy',
     ]
 
     @admin.action(description="Schedule to analyze selected problems.")
-    def action_schedule_analyze(self, request, queryset: QuerySet[proxy.Problem]):
+    def action_schedule_analyze(self, request, queryset: QuerySet[models.ProblemDAO]):
         for obj in queryset:
-            obj.analyze()
+            analyzer.schedule_analysis(obj.pk)
 
     @admin.action(description="Create copies of selected problems.")
-    def action_copy(self, request, queryset: QuerySet[proxy.Problem]):
+    def action_copy(self, request, queryset: QuerySet[models.ProblemDAO]):
         for obj in queryset:
-            proxy.Problem.objects.create(
+            models.ProblemDAO.objects.create(
                 title=obj.title,
                 link=obj.link,
                 description=obj.description,
@@ -49,47 +48,47 @@ class ProblemModelAdmin(admin.ModelAdmin):
             )
 
 
-@admin.register(proxy.ProblemAnalysis)
+@admin.register(models.ProblemAnalysisDAO)
 class ProblemAnalysisModelAdmin(admin.ModelAdmin):
     list_display = [
-        proxy.ProblemAnalysis.field_name.PROBLEM,
-        proxy.ProblemAnalysis.field_name.DIFFICULTY,
+        models.ProblemAnalysisDAO.field_name.PROBLEM,
+        models.ProblemAnalysisDAO.field_name.DIFFICULTY,
         'get_time_complexity',
         'get_tags',
         'get_hint_steps',
-        proxy.ProblemAnalysis.field_name.CREATED_AT,
+        models.ProblemAnalysisDAO.field_name.CREATED_AT,
     ]
     search_fields = [
-        proxy.ProblemAnalysis.field_name.PROBLEM+'__'+proxy.Problem.field_name.TITLE,
-        proxy.ProblemAnalysis.field_name.TIME_COMPLEXITY,
+        models.ProblemAnalysisDAO.field_name.PROBLEM+'__'+models.ProblemDAO.field_name.TITLE,
+        models.ProblemAnalysisDAO.field_name.TIME_COMPLEXITY,
     ]
-    ordering = ['-'+proxy.ProblemAnalysis.field_name.CREATED_AT]
+    ordering = ['-'+models.ProblemAnalysisDAO.field_name.CREATED_AT]
 
     @admin.display(description='Time complexity')
-    def get_time_complexity(self, obj: proxy.ProblemAnalysis) -> str:
+    def get_time_complexity(self, obj: models.ProblemAnalysisDAO) -> str:
         return f'O({obj.time_complexity})'
 
     @admin.display(description='Tags')
-    def get_tags(self, analysis: proxy.ProblemAnalysis) -> str:
-        tag_keys = proxy.ProblemAnalysisTag.objects.filter(**{
-            proxy.ProblemAnalysisTag.field_name.ANALYSIS: analysis,
+    def get_tags(self, analysis: models.ProblemAnalysisDAO) -> str:
+        tag_keys = models.ProblemAnalysisTagDAO.objects.filter(**{
+            models.ProblemAnalysisTagDAO.field_name.ANALYSIS: analysis,
         }).select_related(
-            proxy.ProblemAnalysisTag.field_name.TAG,
+            models.ProblemAnalysisTagDAO.field_name.TAG,
         ).values_list(
-            proxy.ProblemAnalysisTag.field_name.TAG+'__'+BOJTag.field_name.KEY,
+            models.ProblemAnalysisTagDAO.field_name.TAG+'__'+BOJTag.field_name.KEY,
             flat=True,
         )
         return ', '.join(tag_keys)
 
     @admin.display(description='Hint steps')
-    def get_hint_steps(self, obj: proxy.ProblemAnalysis) -> int:
+    def get_hint_steps(self, obj: models.ProblemAnalysisDAO) -> int:
         return len(obj.hints)
 
 
-@admin.register(proxy.ProblemAnalysisTag)
+@admin.register(models.ProblemAnalysisTagDAO)
 class ProblemAnalysisTagModelAdmin(admin.ModelAdmin):
     list_display = [
-        proxy.ProblemAnalysisTag.field_name.ANALYSIS,
-        proxy.ProblemAnalysisTag.field_name.TAG,
+        models.ProblemAnalysisTagDAO.field_name.ANALYSIS,
+        models.ProblemAnalysisTagDAO.field_name.TAG,
     ]
-    ordering = [proxy.ProblemAnalysisTag.field_name.ANALYSIS]
+    ordering = [models.ProblemAnalysisTagDAO.field_name.ANALYSIS]
