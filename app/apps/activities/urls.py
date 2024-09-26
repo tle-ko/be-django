2f7f9f733179c08a1c3a4a0682c826e58869497c
Hepheir <hepheir@gmail.com>
2024-09-26 10:11:44 +0900

    refactor(activities): DRF를 활용하여 코드 가독성 향상 + 혼돈을 야기하는 CrewActivitySubmission 모델 제거

--- app/apps/activities/urls.py
+++ app/apps/activities/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path("crew/activity/<int:activity_id>", views.CrewActivityRetrieveAPIView.as_view()),
+    path("crew/activity/problem/<int:problem_id>", views.CrewActivityProblemRetrieveAPIView.as_view()),
 ]
